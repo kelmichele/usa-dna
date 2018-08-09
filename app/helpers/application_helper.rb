b5ab9b1cd2ab4454b95dc71ecc9e94808104449e
Kelli Parris <kelli@hsdesignhouse.com>
2018-08-09 15:00:21 -0500

    removed secrets file, to see if map works on heroku

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+	def phone
+	  number_to_phone(18667504773, delimiter: ' ', area_code: true,  class: "phone-link", id: "number_link", onclick: "ga('send', 'event', 'Phone Number', 'Clicked to Call', 'Number on Website', '1')")
+	end
 end
