2be4880555316e5c178826f045e04b7e490e3dde
Kelli Parris <kelli@hsdesignhouse.com>
2018-08-09 08:45:41 -0500

    scss, images, basic pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'static_pages#home'
+  get 'info', to: 'static_pages#info'
+	get 'test-page', to: 'static_pages#test-page'
+	get 'options', to: 'static_pages#options'
+	get '/our-process', to: 'static_pages#our_process', as: 'our_process'
+	get 'testing', to: 'static_pages#testing'
 end
