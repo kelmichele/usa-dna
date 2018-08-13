de2d2abcf92ec87be797ecce32fad9c5621238a9
Kelli Parris <kelli@hsdesignhouse.com>
2018-08-13 11:03:35 -0500

    updated locations form

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,7 +1,7 @@
 class LocationsController < ApplicationController
 	before_action :set_location, only: [:edit, :show, :update, :destroy]
-  before_action :authenticate_user!, except: [:index, :show]
-  before_action :require_admin, except: [:index, :show]
+  # before_action :authenticate_user!, except: [:index, :show]
+  # before_action :require_admin, except: [:index, :show]
 
 	def index
 		@locations = if params[:l]
