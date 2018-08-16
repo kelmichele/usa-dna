bc1041f4275cbb298a4dc78bc2df9c6678085ad9
Kelli Parris <kelli@hsdesignhouse.com>
2018-08-16 08:57:05 -0500

    a few new images, new phone number

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
 	def phone
-	  number_to_phone(18667504773, delimiter: ' ', area_code: true,  class: "phone-link", id: "number_link", onclick: "ga('send', 'event', 'Phone Number', 'Clicked to Call', 'Number on Website', '1')")
+	  number_to_phone(8662125426, delimiter: ' ', area_code: true,  class: "phone-link", id: "number_link", onclick: "ga('send', 'event', 'Phone Number', 'Clicked to Call', 'Number on Website', '1')")
 	end
 end
